5eaf9327936aa528a1b16acdf10132752c8b29c0
tonnipaul <ariyoadeoyepaul@gmail.com>
2025-01-03 00:58:31 +0100

    feat: add more svg icon config

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,8 +1,10 @@
 const BRAND_NAME = 'Wisdom house';
 
+const DATE_FORMAT = 'D MMM, YYYY';
+
 const INSTAGRAM = '#';
 const TWITTER = '#';
 const FACEBOOK = '#';
 const TIKTOK = '#';
 
-export { INSTAGRAM, TWITTER, FACEBOOK, TIKTOK, BRAND_NAME };
+export { INSTAGRAM, TWITTER, FACEBOOK, TIKTOK, BRAND_NAME, DATE_FORMAT };
