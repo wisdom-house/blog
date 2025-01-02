a6c802972a7703c4208f6874e9cf1d440a383b6c
tonnipaul <ariyoadeoyepaul@gmail.com>
2025-01-02 12:57:41 +0100

    feat: add footer
    add more svg icon sprite and types

--- types/icon.type.ts
+++ types/icon.type.ts
@@ -1,3 +1,20 @@
-export type IconName = 'search' | 'menu' | 'close';
+export type IconName =
+  | 'search'
+  | 'menu'
+  | 'close'
+  | 'facebook'
+  | 'twitter'
+  | 'instagram'
+  | 'tiktok'
+  | 'linkedin';
 
-export const icons: IconName[] = ['menu', 'menu', 'close'];
+export const icons: IconName[] = [
+  'menu',
+  'menu',
+  'close',
+  'facebook',
+  'twitter',
+  'instagram',
+  'tiktok',
+  'linkedin',
+];
---
+++ utils/constants.ts
@@ -0,0 +1,8 @@
+const BRAND_NAME = 'Wisdom house';
+
+const INSTAGRAM = '#';
+const TWITTER = '#';
+const FACEBOOK = '#';
+const TIKTOK = '#';
+
+export { INSTAGRAM, TWITTER, FACEBOOK, TIKTOK, BRAND_NAME };
