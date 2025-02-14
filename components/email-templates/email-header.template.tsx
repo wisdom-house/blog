cf2e208346cd403e2fd16ade4312e8ea0b823c52
tonnipaul <ariyoadeoyepaul@gmail.com>
2025-02-14 10:13:32 +0100

    feat: create email template header and footer component

---
+++ components/email-templates/email-header.template.tsx
@@ -0,0 +1,11 @@
+import { Img, Section } from '@react-email/components';
+
+export const EmailHeader = () => (
+  <Section className="text-center mb-8">
+    <Img
+      src="https://res.cloudinary.com/dhleu6fhi/image/upload/v1739479847/logo-nbg_og12bw.png"
+      alt="Wisdom House International"
+      className="mx-auto w-32"
+    />
+  </Section>
+);
